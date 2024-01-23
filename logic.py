7135eef4f9472404e4f07418ad4a77ab3024feca
Jolitaviso <you@example.com>
2024-01-23 14:54:39 +0200

    pataisyta

--- logic.py
+++ logic.py
@@ -20,12 +20,12 @@ def add_new_2(mat):
 
 	r = random.randint(0, 3)
 	c = random.randint(0, 3)
-
-	while(mat[r] != 0):
+     
+	while(mat[r][c] != 0):
 		r = random.randint(0, 3)
 		c = random.randint(0, 3)
 
-	mat[r] = 2
+	mat[r][c] = 2
 
 def get_current_state(mat):
 	
